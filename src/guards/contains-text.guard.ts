d89ff5aec1171cafcf2d89036f53560a9e880682
Harold Waters <waters.harold@gmail.com>
2020-11-20 23:41:24 -0600

    feat(src/guards): add contains-text guard

--- src/guards/contains-text.guard.ts
+++ src/guards/contains-text.guard.ts
@@ -4,14 +4,14 @@ import { DiscordContext } from '../discord-context';
 
 @Injectable()
 export class ContainsText implements CanActivate {
-  private readonly re: RegExp;
-  constructor(text: RegExp | string) {
-    this.re = _.isString(text) ? new RegExp(text, 'mi') : (text as RegExp);
-  }
-  canActivate(context: ExecutionContext): boolean {
-    const [, ctx] = context.getArgs();
-    const cleanContent = (ctx as DiscordContext).getMessage().cleanContent;
+    private readonly re: RegExp;
+    constructor(text: RegExp | string) {
+        this.re = _.isString(text) ? new RegExp(text, 'mi') : (text as RegExp);
+    }
+    canActivate(context: ExecutionContext): boolean {
+        const [, ctx] = context.getArgs();
+        const cleanContent = (ctx as DiscordContext).getMessage().cleanContent;
 
-    return this.re.test(cleanContent);
-  }
+        return this.re.test(cleanContent);
+    }
 }
