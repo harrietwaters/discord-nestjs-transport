cc842d29ba9fb9e7a21e00f5ee99979403a8f0a3
Harold Waters <waters.harold@gmail.com>
2020-11-20 23:43:22 -0600

    refactor(discordevent.ts): bump random bytes to 16

--- src/decorators/DiscordEvent.ts
+++ src/decorators/DiscordEvent.ts
@@ -4,9 +4,9 @@ import { randomBytes } from 'crypto';
 import * as Discord from 'discord.js';
 
 export function DiscordEvent(event: keyof Discord.ClientEvents) {
-  // This is a dumb hack - basically you can only have one hander per pattern
-  // so we need make the patterns unique in order set multiple handers. We'll
-  // undo this in the transport before registering the handler
-  const uniqifier = randomBytes(4).toString('hex');
-  return applyDecorators(EventPattern(`${event}-${uniqifier}`));
+    // This is a dumb hack - basically you can only have one hander per pattern
+    // so we need make the patterns unique in order set multiple handers. We'll
+    // undo this in the transport before registering the handler
+    const uniqifier = randomBytes(16).toString('hex');
+    return applyDecorators(EventPattern(`${event}-${uniqifier}`));
 }
